de7fb16f508b024c7407d0fe51721e3ceccf9596
GitHub Action <action@github.com>
2023-04-29 06:08:32 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "coding-with-ai"
--- src/main/kotlin/com/github/fukata/codingwithai/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fukata/codingwithai/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.fukata.codingwithai.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fukata.codingwithai.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
